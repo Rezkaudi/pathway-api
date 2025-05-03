5a7207621f77a6d6b4637991416cfcdcbb134e4b
rezkaudi <audirezk@gmail.com>
2025-05-03 12:51:43 +0300

    add status code in logger middleware

--- src/presentation/middleware/logger.middleware.ts
+++ src/presentation/middleware/logger.middleware.ts
@@ -1,6 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const logger = (req: Request, res: Response, next: NextFunction) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+    const start = new Date().toISOString();
+
+    res.on('finish', () => {
+        const status = res.statusCode;
+        console.log(`[${start}] ${req.method} ${req.originalUrl} - ${status}`);
+    });
+
     next();
 };
